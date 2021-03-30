8b825b4b8c7a81e9b93646e5707413df964e632e
ALOISIO SAMPAIO CAIRO <aloisiocairo@gmail.com>
2021-03-30 07:37:09 -0300

    Implementado a função [removeOldPhot] na const [PhotosUpload]

--- public/scripts.js
+++ public/scripts.js
@@ -9,15 +9,20 @@ for (item of menuItens) {
 }
 
 const PhotosUpload = {
+    input: "",
     preview: document.querySelector('#photos-preview'),
     uploadLimit: 5,
+    files: [],
     handleFileInput(event) {
         const { files: fileList } = event.target
+        PhotosUpload.input = event.target
 
         if (PhotosUpload.hasLimit(event))
             return
 
         Array.from(fileList).forEach(file => {
+            PhotosUpload.files.push(file) /* Carrega todas as imagens para dentro de um array */
+
             const reader = new FileReader()
 
             reader.onload = () => {
@@ -31,10 +36,12 @@ const PhotosUpload = {
 
             reader.readAsDataURL(file)
         })
+
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
     },
     hasLimit(event) {
-        const { uploadLimit } = PhotosUpload
-        const { files: fileList } = event.target
+        const { uploadLimit, input, preview } = PhotosUpload
+        const { files: fileList } = input
 
         if (fileList.length > uploadLimit) {
             alert(`Envie no máximo ${uploadLimit} fotos`)
@@ -42,13 +49,34 @@ const PhotosUpload = {
             return true
         }
 
+        const photosDiv = []
+        preview.childNodes.forEach(item => {
+            if (item.classList && item.classList.value == "photo")
+                photosDiv.push(item)
+        })
+
+        const totalPhotos = fileList.length + photosDiv.length
+        if (totalPhotos > uploadLimit) {
+            alert("Você atingiu o limite máximo de fotos")
+            event.preventDefault()
+
+            return true
+        }
+
         return false
+    },
+    getAllFiles() {
+        const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
+
+        PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
+
+        return dataTransfer.files
     },
     getContainer(image) {
         const div = document.createElement('div')
         div.classList.add('photo')
 
-        div.onclick = () => alert('remover foto')
+        div.onclick = PhotosUpload.removePhoto
 
         div.appendChild(image)
 
@@ -62,6 +90,29 @@ const PhotosUpload = {
         button.innerHTML = "close"
         return button
     },
+    removePhoto(event) {
+        const photoDiv = event.target.parentNode // <div class="photo">
+        const photosArray = Array.from(PhotosUpload.preview.children)
+        const index = photosArray.indexOf(photoDiv)
+
+        PhotosUpload.files.splice(index, 1)
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
+        photoDiv.remove()
+    },
+    removeOldPhoto(event) {
+        const photoDiv = event.target.parentNode
+
+        if (photoDiv.id) {
+            const removedFiles = document.querySelector('input[name="removed_files"]')
+
+            if (removedFiles) {
+                removedFiles.value += `${photoDiv.id},`
+            }
+        }
+
+        photoDiv.remove()
+    }
 }
 
 
